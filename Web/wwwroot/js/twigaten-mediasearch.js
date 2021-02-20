e21cbe41035fcefa93286dd8d2bf0c960295b1ef
204504bySE <204504byse@204504byse.info>
2021-02-20 12:03:44 +0900

    classList.toggleである必要はないめう

--- Web/wwwroot/js/twigaten-mediasearch.js
+++ Web/wwwroot/js/twigaten-mediasearch.js
@@ -7,12 +7,12 @@
   mediaDrop.addEventListener('dragover', function (event) {
     event.preventDefault();
     event.stopPropagation();
-    event.currentTarget.classList.toggle('dragging', true);
+    event.currentTarget.classList.add('dragging');
   });
   mediaDrop.addEventListener('dragenter', function (event) {
     event.preventDefault();
     event.stopPropagation();
-    event.currentTarget.classList.toggle('dragging', true);
+    event.currentTarget.classList.add('dragging');
   });
   mediaDrop.addEventListener('dragleave', function (event) {
     event.preventDefault();
@@ -27,7 +27,7 @@
     if (event.dataTransfer.files && 0 < event.dataTransfer.files.length) {
       const file = event.dataTransfer.files[0];
       if (file.type.startsWith('image/')) {
-        event.currentTarget.classList.toggle('dropped', true);
+        event.currentTarget.classList.add('dropped');
         mediaInput.files = event.dataTransfer.files;
         mediaForm.submit();
       }
@@ -35,12 +35,12 @@
   });
   //ファイルを選択したときもPOST
   mediaInput.addEventListener('change', function () {
-    mediaDrop.classList.toggle('dropped', true);
+    mediaDrop.classList.add('dropped');
     mediaForm.submit();
   });
 
   //戻るボタン等でこのページに来たら色を戻す
   window.addEventListener('pageshow', function () {
-    mediaDrop.classList.toggle('dropped', false);
+    mediaDrop.classList.remove('dropped');
   });
 })();
