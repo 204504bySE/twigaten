6c626a9df830eb520a183ae7106bc7aa381515b7
204504bySE <204504byse@204504byse.info>
2022-06-17 11:38:59 +0900

    各ツイートのページで画像が表示されないのを修正

--- Web/wwwroot/js/twigaten-tweet.js
+++ Web/wwwroot/js/twigaten-tweet.js
@@ -37,6 +37,7 @@
     },
     false
   );
+
   //convert unixtime
   Array.prototype.forEach.call(document.getElementsByClassName('twigaten-unixtime'), function (x) {
     x.textContent = new Date(x.dataset.unixtime * 1000).toLocaleString();
